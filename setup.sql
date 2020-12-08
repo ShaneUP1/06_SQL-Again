39dbc56062455d46813481bc36be4c127faa9fc6
Shane Upchurch <shaneupchurch@gmail.com>
2020-12-08 17:53:10 -0600

    get and post routes work

---
+++ setup.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    author TEXT NOT NULL,
+    good BOOLEAN
+);
